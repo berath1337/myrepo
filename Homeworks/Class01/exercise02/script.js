f8abf09c4484973c18bbd4f078226729218b4b15
berath1337 <153142638+berath1337@users.noreply.github.com>
2023-12-15 14:50:29 +0100

    Delete Homeworks directory

--- Homeworks/Class01/exercise02/script.js
+++
@@ -1,7 +0,0 @@
-//Homework 2
-console.log("This js program takes minutes and converts them to seconds");
-const conversionValue=60;
-let minutes=20;
-let conversionResult= minutes*conversionValue;
-console.log('It is done by multiplication of ', conversionValue ,'and', minutes);
-console.log('The result is ', conversionResult);
