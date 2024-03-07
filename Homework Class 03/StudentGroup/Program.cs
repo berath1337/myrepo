44ecc9c8c4dbe2fc253046940aad954f82525913
berath1337 <berat1337@hotmail.com>
2024-03-08 00:57:34 +0100

    hw

---
+++ Homework Class 03/StudentGroup/Program.cs
@@ -0,0 +1,28 @@
+﻿string[] studentsG1 = new string[] { "Zdravko", "Stanko", "Petko", "Brajko", "Marijan" };
+string[] studentsG2 = new string[] { "Petar", "Pavle", "Kristijan", "Andrej", "Mitko" };
+
+Console.WriteLine("Enter student group: ( there are 1 and 2 )");
+string input = Console.ReadLine();
+
+if (input == "1")
+{
+    Console.WriteLine($"The students in G1 are: ");
+    foreach (string student in studentsG1)
+    {
+        Console.WriteLine(student);
+    }
+}
+else if (input == "2")
+{
+    Console.WriteLine($"The students in G2 are ");
+
+    foreach (string student in studentsG2)
+
+    { 
+        Console.WriteLine(student); 
+    }
+}
+else
+{
+    Console.WriteLine("You have entered an invalid group");
+}
---
+++ Homework Class 03/SumOfEven/Program.cs
@@ -0,0 +1,37 @@
+﻿//Make a console application called SumOfEven. Inside it create an array of 6 integers. Find and print the sum of the even numbers from the array:
+
+//Test Data:
+//Enter integer no.1:
+//4
+//Enter integer no.1:
+//3
+//Enter integer no.1:
+//7
+//Enter integer no.1:
+//3
+//Enter integer no.1:
+//2
+//Enter integer no.1:
+//8
+//Expected Output:
+//The result is: 14
+
+
+int[] ints = new int[6];
+
+for (int i = 0; i < ints.Length; i++)
+{
+    Console.WriteLine("Enter integer no: " + (i+1));
+    int.TryParse(Console.ReadLine(), out ints[i]);
+
+}
+
+int sum = 0;
+foreach (int num in ints)
+{
+    if (num %2 == 0)
+    { 
+        sum += num; 
+    }
+}
+Console.WriteLine("The result is: " + sum);
